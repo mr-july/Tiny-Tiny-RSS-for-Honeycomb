e8fe62b7be1db05176d4484f8019773def8daa70
Igor Lubimov <mr_july@mail.ru>
2013-06-28 16:47:34 +0200

    set database varsion to 5: - add lang column to feeds table; - implement upgrade from version 4 to 5.

--- src/org/fox/ttrss/util/DatabaseHelper.java
+++ src/org/fox/ttrss/util/DatabaseHelper.java
@@ -10,12 +10,13 @@ public class DatabaseHelper extends SQLiteOpenHelper {
 	@SuppressWarnings("unused")
 	private final String TAG = this.getClass().getSimpleName();
 	public static final String DATABASE_NAME = "OfflineStorage.db";
-	public static final int DATABASE_VERSION = 4;
-	
+	public static final int DATABASE_VERSION = 5;
+	public static final String DEFAULT_LANG = "en-gb";
+
 	public DatabaseHelper(Context context) {
 		super(context, DATABASE_NAME, null, DATABASE_VERSION);
 	}
-	
+
 	@Override
 	public void onCreate(SQLiteDatabase db) {
 		db.execSQL("DROP VIEW IF EXISTS cats_unread;");
@@ -24,19 +25,20 @@ public class DatabaseHelper extends SQLiteOpenHelper {
 		db.execSQL("DROP TABLE IF EXISTS categories;");
 		db.execSQL("DROP TABLE IF EXISTS feeds;");
 		db.execSQL("DROP TABLE IF EXISTS articles;");
-		
+
 		db.execSQL("CREATE TABLE IF NOT EXISTS feeds (" +
                 BaseColumns._ID + " INTEGER PRIMARY KEY AUTOINCREMENT," +
                 "feed_url TEXT, " +
                 "title TEXT, " +
                 "has_icon BOOLEAN, " +
-                "cat_id INTEGER" +
-                ");");                
+                "cat_id INTEGER, " +
+                "lang TEXT NOT NULL DEFAULT '" + DEFAULT_LANG + "'" +
+                ");");
 
 		db.execSQL("CREATE TABLE IF NOT EXISTS categories (" +
                 BaseColumns._ID + " INTEGER PRIMARY KEY AUTOINCREMENT," +
                 "title TEXT" +
-                ");");                
+                ");");
 
 		db.execSQL("CREATE TABLE IF NOT EXISTS articles (" +
                 BaseColumns._ID + " INTEGER PRIMARY KEY AUTOINCREMENT," +
@@ -55,23 +57,23 @@ public class DatabaseHelper extends SQLiteOpenHelper {
                 "selected BOOLEAN, " +
                 "modified BOOLEAN" +
                 ");");
-		
+
 		db.execSQL("CREATE TRIGGER articles_set_modified UPDATE OF marked, published, unread ON articles " +
 		"BEGIN " +
 		" UPDATE articles SET modified = 1 WHERE " + BaseColumns._ID + " = " + "OLD." + BaseColumns._ID + "; " +
 		"END;");
-		
+
 		db.execSQL("CREATE VIEW feeds_unread AS SELECT feeds."+BaseColumns._ID+" AS "+BaseColumns._ID+", " +
 				"feeds.title AS title, " +
 				"cat_id, " +
 				"SUM(articles.unread) AS unread FROM feeds " +
 				"LEFT JOIN articles ON (articles.feed_id = feeds."+BaseColumns._ID+") " +
 				"GROUP BY feeds."+BaseColumns._ID+", feeds.title;");
-		
+
 		//sqlite> select categories._id,categories.title,sum(articles.unread) from categories left j
 		//oin feeds on (feeds.cat_id = categories._id) left join articles on (articles.feed_id = fee
 		//ds._id) group by categories._id;
-		
+
 		db.execSQL("CREATE VIEW cats_unread AS SELECT categories."+BaseColumns._ID+" AS "+BaseColumns._ID+", " +
 				"categories.title AS title, " +
 				"SUM(articles.unread) AS unread FROM categories " +
@@ -83,7 +85,14 @@ public class DatabaseHelper extends SQLiteOpenHelper {
 
 	@Override
 	public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
-		onCreate(db);
+    if (oldVersion == 4 && newVersion == 5)
+    {
+      db.execSQL ("ALTER TABLE feeds " +
+        "ADD COLUMN lang TEXT NOT NULL DEFAULT '" + DEFAULT_LANG + "';");
+    } else
+    {
+      onCreate (db);
+    }
 	}
 
 }
